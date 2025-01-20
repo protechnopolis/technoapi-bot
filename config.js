98f4d024685775c835d303ae04186ebe1537b8eb
protechnopolis <146369803+protechnopolis@users.noreply.github.com>
2025-01-20 14:08:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "MTI5NTMzMzk5NTUwNDA3ODg5OQ.GmzNEh.noSMOg_SV9P7njrS8s--OYC9TubR2XS0PIPxHc",
+    token: "YOUR TOKEN",
     prefix: "&",
     botStatus: "By Protechnopolis",
     footer: "Created by Protechnopolis"
