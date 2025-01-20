dc7a733cdf9e7bbc64496573fcba19aced1fd243
protechnopolis <146369803+protechnopolis@users.noreply.github.com>
2025-01-20 14:08:30 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "MTI5NTMzMzk5NTUwNDA3ODg5OQ.GmzNEh.noSMOg_SV9P7njrS8s--OYC9TubR2XS0PIPxHc",
+    prefix: "&",
+    botStatus: "By Protechnopolis",
+    footer: "Created by Protechnopolis"
+}
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Merci de ne pas s'apprioprier le script
+
+## Ce script a été fait par protechnopolis.
+
+Si vous le faites quand meme, vous serez blacklist de l'api, et votre bot sera inutilisable
